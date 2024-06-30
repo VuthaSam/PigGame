48b28cb3587af47d4e93ae34e4d2664034680bff
Vutha Sam <131248312+VuthaSam@users.noreply.github.com>
2024-06-30 18:18:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+'use strict';
+// Elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+// Var
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+// Function :
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+const btnRollDice = function () {
+  if (playing) {
+    // Random number for dice
+    const dice = Math.floor(Math.random() * 6) + 1;
+
+    //Display Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // Is it a 1?
+    if (dice !== 1) {
+      // add dice roll to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch player
+      switchPlayer();
+    }
+  }
+};
+
+const btnHoldDice = function () {
+  if (playing) {
+    // 1. add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Score >= 100?
+    if (scores[activePlayer] >= 100) {
+      // Y : Current player wins
+      diceEl.classList.add('hidden');
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // N : Switch Player
+      switchPlayer();
+    }
+  }
+};
+
+const btnNewGame = function () {
+  playing = true;
+  currentScore = 0;
+  scores[0] = 0;
+  scores[1] = 0;
+  activePlayer = 0;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+// The Three Button :
+btnRoll.addEventListener('click', btnRollDice);
+btnHold.addEventListener('click', btnHoldDice);
+btnNew.addEventListener('click', btnNewGame);
